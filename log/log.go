db10d9a822b1b7983bab6df0b9627f4848f45fc4
Serhan Şen <mail@serhan-sen.de>
2015-06-26 22:41:31 +0200

    Prevent logger console output on initialization.

--- log/log.go
+++ log/log.go
@@ -24,6 +24,4 @@ func init() {
 	Info = log.New(infoHandle, "INFO:    ", log.Ldate|log.Ltime|log.Lshortfile)
 	Warning = log.New(warningHandle, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Error = log.New(errorHandle, "ERROR:   ", log.Ldate|log.Ltime|log.Lshortfile)
-
-	Trace.Println("Logger initialized")
 }
